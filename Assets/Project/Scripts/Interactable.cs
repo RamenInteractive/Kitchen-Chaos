99ca256f803b4397667076396a458231d8b6288f
ColtKing <king.colt@yahoo.ca>
2017-10-04 14:56:43 -0700

    Added some base cut food models and a raycasting script

---
+++ Assets/Project/Scripts/Interactable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    string text;
+
+    public string Text
+    {
+        get { return text; }
+        set { text = value; }
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
