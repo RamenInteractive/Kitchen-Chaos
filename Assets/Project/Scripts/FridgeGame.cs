85b8ee67d17aa37aee6e32e7e0d395c6cb31b3e1
Aaron Freytag <aaroncfreytag@gmail.com>
2017-10-09 23:45:17 -0700

    Created FridgeGame class for the fridge minigame

---
+++ Assets/Project/Scripts/FridgeGame.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class FridgeGame : Minigame
+{
+    new protected void Start() {
+        base.Start();
+    }
+
+    new protected void Update() {
+        base.Update();
+    }
+
+    public override void complete() {
+    }
+}
