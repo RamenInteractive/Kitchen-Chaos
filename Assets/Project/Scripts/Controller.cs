4ea3752690217fada4c3240afb6f23363dd34cde
Aaron Freytag <aaroncfreytag@gmail.com>
2017-10-29 23:52:30 -0700

    Finished basic controller class commands.

--- Assets/Project/Scripts/Controller.cs
+++ Assets/Project/Scripts/Controller.cs
@@ -2,15 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Controller : MonoBehaviour
+public class Controller
 {
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    Dictionary<string, string> buttons;
+    Dictionary<string, string> axes;
+    
+    public bool GetButton(string name) {
+        if (buttons.ContainsKey(name)) {
+            return Input.GetButton(buttons[name]);
+        }
+        else {
+            throw new System.Exception("No controller button of name \"" + name + "\" found");
+        }
+    }
+
+    public bool GetButtonDown(string name) {
+        if (buttons.ContainsKey(name)) {
+            return Input.GetButtonDown(buttons[name]);
+        }
+        else {
+            throw new System.Exception("No controller button of name \"" + name + "\" found");
+        }
+    }
+
+    public bool GetButtonUp(string name) {
+        if (buttons.ContainsKey(name)) {
+            return Input.GetButtonUp(buttons[name]);
+        }
+        else {
+            throw new System.Exception("No controller button of name \"" + name + "\" found");
+        }
+    }
+
+    public float GetAxis(string name) {
+        if (axes.ContainsKey(name)) {
+            return Input.GetAxis(axes[name]);
+        }
+        else {
+            throw new System.Exception("No controller axis of name \"" + name + "\" found");
+        }
+    }
+
+    public float GetAxisRaw(string name) {
+        if (axes.ContainsKey(name)) {
+            return Input.GetAxisRaw(axes[name]);
+        }
+        else {
+            throw new System.Exception("No controller axis of name \"" + name + "\" found");
+        }
+    }
 }
