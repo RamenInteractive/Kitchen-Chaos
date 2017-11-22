0476286912a983f9f56a9e13745a7e67e3d844ea
Aaron Freytag <aaroncfreytag@gmail.com>
2017-11-21 19:10:09 -0800

    Added some controllers

--- Assets/Project/Scripts/Menu/MenuController.cs
+++ Assets/Project/Scripts/Menu/MenuController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MenuController : MonoBehaviour {
 
-    private Screen currentScreen;
+    private MenuScreen currentScreen;
 
 	// Use this for initialization
 	void Start () {
---
+++ Assets/Project/Scripts/Menu/MenuScreen.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+abstract class MenuScreen {
+    /*
+    private float deadZone = 3.0f;
+    protected float GetAnyAxis(string axisName) {
+        float rAxis = 0;
+        for (int i = 0; i < 9; i++) {
+            if (i == 0) { // Keyboard
+
+            }
+            else {
+
+            }
+            if (rAxis > deadZone || rAxis < -deadZone) {
+                break;
+            }
+        }
+        return rAxis;
+    }
+
+    protected int? GetAnyAccept() {
+
+    }
+
+    protected int? GetAnyBack() {
+
+    }
+    */
+}
