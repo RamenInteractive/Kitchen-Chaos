1a1575818bb68fc2f4ec1a374de45499bf1fe693
Aaron Freytag <aaroncfreytag@gmail.com>
2017-11-21 18:28:21 -0800

    Added basic menu visuals

---
+++ Assets/MenuController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour {
+
+    private Screen currentScreen;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
