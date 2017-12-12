abe0138188e9ae37c77e3b4d8bb8e49324e4c787
Aaron Freytag <aaroncfreytag@gmail.com>
2017-12-12 00:19:11 -0800

    Orders now spontaneously combust

---
+++ Assets/Project/Scripts/DoNotRotate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoNotRotate : MonoBehaviour {
+    Quaternion originalRotation;
+	// Use this for initialization
+	void Start () {
+        transform.rotation *= Quaternion.Inverse(GetComponentInParent<Transform>().rotation);
+        originalRotation = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.rotation = originalRotation;
+	}
+}
